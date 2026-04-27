2443f8f52239bc996c3ada292127474bdc5fc57b
kaushikpathakk <kaushikpathak@example.com>
2026-04-27 11:33:40 +0530

    Updated code with new changes

--- merge.cpp
+++ merge.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
-// int time = 0;
+
 class Process
 {
 public:
     int at, bt, ct, tat, wt, id, completed;
+
     void input(int i)
     {
         id = i;
-        cout << " Enter the value of AT and BT of process " << id << endl;
+        cout << "Enter AT and BT of process " << id << endl;
         cin >> at >> bt;
     }
 };
+
+// ===================== SJF =====================
 void CalculateSJF(Process p[], int n)
 {
-    int time = 0;
-    int completed = 0;
-    int idle_time = 0;
+    int time = 0, completed = 0, idle_time = 0;
     float total_tat = 0, total_wt = 0;
+
+    int order[50], k = 0; // store execution order
+
     while (completed < n)
     {
-        int idx = -1;
-        int minbt = 99999;
+        int idx = -1, minbt = 99999;
 
         for (int i = 0; i < n; i++)
         {
@@ -35,32 +38,15 @@ void CalculateSJF(Process p[], int n)
                 }
             }
         }
-        if (idx == -1)
-        {
-            time++;
-            idle_time++;
-        }
-        else
-        {
-            p[idx].ct = time + p[idx].bt;
-            p[idx].tat = p[idx].ct - p[idx].at;
-            p[idx].wt = p[idx].tat - p[idx].bt;
-            time = p[idx].ct;
-            p[idx].completed = 1;
-            completed++;
-        }
-         
-        cout << "\nGantt Chart:\n| ";
 
         if (idx == -1)
         {
-            cout << "idle | ";
             time++;
             idle_time++;
         }
         else
         {
-            cout << "P" << p[idx].id << " | ";
+            order[k++] = idx;
 
             p[idx].ct = time + p[idx].bt;
             p[idx].tat = p[idx].ct - p[idx].at;
@@ -69,57 +55,31 @@ void CalculateSJF(Process p[], int n)
             time = p[idx].ct;
             p[idx].completed = 1;
             completed++;
-        }
-        cout << "\n0 ";
-        time = 0;
-
-        while (true)
-        {
-            int done = 1;
-
-            for (int i = 0; i < n; i++)
-            {
-                if (p[i].completed == 0)
-                {
-                    done = 0;
-                }
-            }
-
-            if (done)
-                break;
-
-            int idx = -1;
-            int minbt = 99999;
-
-            for (int i = 0; i < n; i++)
-            {
-                if (p[i].at <= time && p[i].completed == 0)
-                {
-                    if (idx == -1 || p[i].bt < minbt || (p[i].bt == minbt && p[i].at < p[idx].at))
-                    {
-                        minbt = p[i].bt;
-                        idx = i;
-                    }    
-                }
-            }
-
-            if (idx == -1)
-            {
-                time++;
-                cout << "   " << time;
-            }
-            else
-            {
-                time += p[idx].bt;
-                cout << "   " << time;
-                p[idx].completed = 1;
-            }
         }
     }
 
-    cout << " Idle Time :" << idle_time << endl;
-    cout << "\nPID\tAT\tBT\tCT\tTAT\tWT\n";
+    //  Gantt Chart
+    cout << "\nGantt Chart:\n| ";
+    for (int i = 0; i < k; i++)
+        cout << "P" << p[order[i]].id << " | ";
 
+    cout << "\n0 ";
+    time = 0;
+
+    for (int i = 0; i < k; i++)
+    {
+        int idx = order[i];
+
+        if (time < p[idx].at)
+            time = p[idx].at;
+
+        time += p[idx].bt;
+        cout << "   " << time;
+    }
+    cout << endl;
+
+    // Result table
+    cout << "\nPID\tAT\tBT\tCT\tTAT\tWT\n";
     for (int i = 0; i < n; i++)
     {
         cout << "P" << p[i].id << "\t"
@@ -132,74 +92,33 @@ void CalculateSJF(Process p[], int n)
         total_tat += p[i].tat;
         total_wt += p[i].wt;
     }
+
     cout << "\nAverage TAT: " << total_tat / n << endl;
     cout << "Average WT: " << total_wt / n << endl;
+    cout << "Idle Time: " << idle_time << endl;
 }
 
+// ===================== FCFS =====================
 void CalculateFCFS(Process p[], int n)
 {
-
     // sort by arrival time
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
             if (p[j].at > p[j + 1].at)
-            {
                 swap(p[j], p[j + 1]);
-            }
         }
     }
 
-    int time = 0;
-    int idle_time = 0;
+    int time = 0, idle_time = 0;
     float total_tat = 0, total_wt = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-
-        if (time < p[i].at)
-        {
-            idle_time += (p[i].at - time);
-            time = p[i].at;
-        }
-
-        p[i].ct = time + p[i].bt;
-        p[i].tat = p[i].ct - p[i].at;
-        p[i].wt = p[i].tat - p[i].bt;
-
-        time = p[i].ct;
-    }
-
-    float cpu_util = ((float)(time - idle_time) / time) * 100;
-    float throughput = (float)n / time;
-
-    cout << "Throughput: " << throughput << endl;
-    cout << "Idle Time: " << idle_time << endl;
-    cout << "CPU Utilisation: " << cpu_util << "%\n";
-
-    cout << "\nPID\tAT\tBT\tCT\tTAT\tWT\n";
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << "P" << p[i].id << "\t"
-             << p[i].at << "\t"
-             << p[i].bt << "\t"
-             << p[i].ct << "\t"
-             << p[i].tat << "\t"
-             << p[i].wt << endl;
-
-        total_tat += p[i].tat;
-        total_wt += p[i].wt;
-    }
-
-    cout << "\nAverage TAT: " << total_tat / n << endl;
-    cout << "Average WT: " << total_wt / n << endl;
-
+    //  Gantt Chart
     cout << "\nGantt Chart:\n| ";
+
     for (int i = 0; i < n; i++)
     {
-
         if (time < p[i].at)
         {
             cout << "idle | ";
@@ -207,7 +126,6 @@ void CalculateFCFS(Process p[], int n)
             time = p[i].at;
         }
 
-        // Gantt chart
         cout << "P" << p[i].id << " | ";
 
         p[i].ct = time + p[i].bt;
@@ -216,24 +134,45 @@ void CalculateFCFS(Process p[], int n)
 
         time = p[i].ct;
     }
+
+    // timeline
     cout << "\n0 ";
     time = 0;
 
     for (int i = 0; i < n; i++)
     {
-
         if (time < p[i].at)
         {
-            cout << "   " << p[i].at;
             time = p[i].at;
+            cout << "   " << time;
         }
 
         time += p[i].bt;
         cout << "   " << time;
     }
     cout << endl;
+
+    // results
+    cout << "\nPID\tAT\tBT\tCT\tTAT\tWT\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "P" << p[i].id << "\t"
+             << p[i].at << "\t"
+             << p[i].bt << "\t"
+             << p[i].ct << "\t"
+             << p[i].tat << "\t"
+             << p[i].wt << endl;
+
+        total_tat += p[i].tat;
+        total_wt += p[i].wt;
+    }
+
+    cout << "\nAverage TAT: " << total_tat / n << endl;
+    cout << "Average WT: " << total_wt / n << endl;
+    cout << "Idle Time: " << idle_time << endl;
 }
 
+// ===================== MAIN =====================
 int main()
 {
     int n;
@@ -249,8 +188,8 @@ int main()
     }
 
     int choice;
-    cout << " CPU SCHEDULING " << endl;
-    cout << " Enter 1 for FCFS and 2 for SJF " << endl;
+    cout << "\nCPU SCHEDULING\n";
+    cout << "Enter 1 for FCFS and 2 for SJF\n";
     cin >> choice;
 
     Process temp[50];
